8f02fb9e1f4ad784b5e4ced8f203b76b67c7c9d9
Emilia Pezda <emilia.ciejka@gmail.com>
2017-07-09 14:08:19 +0100

    add

---
+++ class/User.php
@@ -0,0 +1,130 @@
+<?php
+
+require_once './interfaces/ActiveRecord';
+
+class User implements ActiveRecord, JsonSerializable {
+
+    private $id, $address_id, $name, $surname, $credits, $password;
+
+    function __construct($id, $address_id, $name, $surname, $credits, $password){
+    }
+
+    static function load($id){
+
+    }
+
+    static function loadAll(){
+
+    }
+
+    static function update(){
+
+    }
+
+    static function save(){
+
+    }
+
+    static function delete(){
+
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    private function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAddressId()
+    {
+        return $this->address_id;
+    }
+
+    /**
+     * @param mixed $address_id
+     */
+    public function setAddressId($address_id)
+    {
+        $this->address_id = $address_id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSurname()
+    {
+        return $this->surname;
+    }
+
+    /**
+     * @param mixed $surname
+     */
+    public function setSurname($surname)
+    {
+        $this->surname = $surname;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCredits()
+    {
+        return $this->credits;
+    }
+
+    /**
+     * @param mixed $credits
+     */
+    public function setCredits($credits)
+    {
+        $this->credits = $credits;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param mixed $password
+     */
+    private function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+
+
+}
